38e558a8c71310fc16acb68fb801c600edacbf0f
Akshay Jyothi.P <akshayjyothip@gmail.com>
2025-03-16 23:04:36 +0530

    api calls get ,post,delete,patch

--- src/app.js
+++ src/app.js
@@ -6,20 +6,98 @@ const app = express();
 const User=require('./models/user')
 
 
+app.use(express.json())
+
+
 
 
 app.post('/signup',async(req,res)=>{
 
-    const user=new User({
-        firstName:'Akshay',
-        lastName:'jyothi',
-        emailId:"test@gmail.com",
-        password:'akshay123'
-    })
+    const user=new User(req.body)
 
+   try {
     await user.save()
     res.send('user addded sucefully')
+   } catch (error) {
+    
+    res.status(400).send('error saving  the user'+err.message)
+   }
 })
+
+
+
+
+///feed api to see all tinder user details after login to the application
+
+
+app.get('/user',async(req,res)=>{
+
+    const userEmail=req.body.emailId
+
+    try {
+        
+        const user=await User.find({emailId:userEmail})
+
+        if(user.length===0){
+
+            res.send('sorry user is not found')
+        }
+        else{
+            res.send(user)
+        }
+        
+    } catch (error) {
+        
+        res.status(400).send('error')
+    }
+
+})
+
+
+app.get('/feed',async(req,res)=>{
+
+        try {
+            const users=await User.find({})
+            res.send(users)
+        } catch (error) {
+            
+            res.status(400).send('users not found')
+        }
+})
+
+app.delete('/user',async(req,res)=>{
+
+    const userId=req.body.userId
+   
+    
+    try {
+        
+        const users=await User.findByIdAndDelete(userId)
+        res.send('user deleted sucefully')
+
+    } catch (error) {
+        res.status(400).send('delet not done')
+
+    }
+})
+
+
+app.patch('/user',async(req,res)=>{
+
+    const userId=req.body.userId
+    const data=req.body
+    try {
+
+        const user=await User.findByIdAndUpdate({_id:userId},data)
+        res.send ('updated')
+        
+    } catch (error) {
+        
+        res.status(400).send('delet not done')
+
+    }
+})
+
 
 
 connectDB().then(()=>{
