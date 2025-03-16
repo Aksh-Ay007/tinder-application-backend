06a7e7df26a4d0ef1a68219fa86fef6fff992c25
Akshay Jyothi.P <akshayjyothip@gmail.com>
2025-03-16 20:29:58 +0530

    created first user and test using postman

--- src/app.js
+++ src/app.js
@@ -3,9 +3,27 @@ const connectDB =require('./config/database.js');
 
 const app = express();
 
+const User=require('./models/user')
+
+
+
+
+app.post('/signup',async(req,res)=>{
+
+    const user=new User({
+        firstName:'Akshay',
+        lastName:'jyothi',
+        emailId:"test@gmail.com",
+        password:'akshay123'
+    })
+
+    await user.save()
+    res.send('user addded sucefully')
+})
+
 
 connectDB().then(()=>{
-    console.log('database connection successfully');
+    console.log('database connection successfullyyyyyyyyyy');
 
     app.listen(7777, () => {
         console.log('Server is running ');
@@ -21,4 +39,3 @@ connectDB().then(()=>{
 
 
 
-const PORT = 7777;
