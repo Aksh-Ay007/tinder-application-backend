88e46db6c6435a581cda1779af5aa71f7d02e8e8
Akshay Jyothi.P <akshayjyothip@gmail.com>
2025-03-17 01:02:57 +0530

    validate each field

--- src/app.js
+++ src/app.js
@@ -1,119 +1,95 @@
 const express = require('express');
-const connectDB =require('./config/database.js');
+const connectDB = require('./config/database.js');
 
 const app = express();
 
-const User=require('./models/user')
+const User = require('./models/user');
+const { Error } = require('mongoose');
 
+app.use(express.json());
 
-app.use(express.json())
-
-
-
-
-app.post('/signup',async(req,res)=>{
-
-    const user=new User(req.body)
-
-   try {
-    await user.save()
-    res.send('user addded sucefully')
-   } catch (error) {
-    
-    res.status(400).send('error saving  the user'+err.message)
-   }
-})
-
-
-
-
-///feed api to see all tinder user details after login to the application
-
-
-app.get('/user',async(req,res)=>{
-
-    const userEmail=req.body.emailId
+app.post('/signup', async (req, res) => {
+    const user = new User(req.body);
 
     try {
-        
-        const user=await User.find({emailId:userEmail})
-
-        if(user.length===0){
-
-            res.send('sorry user is not found')
-        }
-        else{
-            res.send(user)
-        }
-        
-    } catch (error) {
-        
-        res.status(400).send('error')
+        await user.save();
+        res.send('User added successfully');
+    } catch (err) {
+        res.status(400).send('Error saving the user: ' + err.message);
     }
+});
 
-})
+app.get('/user', async (req, res) => {
+    const userEmail = req.body.emailId;
 
-
-app.get('/feed',async(req,res)=>{
-
-        try {
-            const users=await User.find({})
-            res.send(users)
-        } catch (error) {
-            
-            res.status(400).send('users not found')
-        }
-})
-
-app.delete('/user',async(req,res)=>{
-
-    const userId=req.body.userId
-   
-    
     try {
-        
-        const users=await User.findByIdAndDelete(userId)
-        res.send('user deleted sucefully')
+        const user = await User.find({ emailId: userEmail });
 
+        if (user.length === 0) {
+            res.send('Sorry, user is not found');
+        } else {
+            res.send(user);
+        }
     } catch (error) {
-        res.status(400).send('delet not done')
-
+        res.status(400).send('Error: ' + error.message);
     }
-})
+});
 
-
-app.patch('/user',async(req,res)=>{
-
-    const userId=req.body.userId
-    const data=req.body
+app.get('/feed', async (req, res) => {
     try {
-
-        const user=await User.findByIdAndUpdate({_id:userId},data)
-        res.send ('updated')
-        
+        const users = await User.find({});
+        res.send(users);
     } catch (error) {
-        
-        res.status(400).send('delet not done')
-
+        res.status(400).send('Users not found: ' + error.message);
     }
-})
+});
 
+app.delete('/user', async (req, res) => {
+    const userId = req.body.userId;
 
+    try {
+        await User.findByIdAndDelete(userId);
+        res.send('User deleted successfully');
+    } catch (error) {
+        res.status(400).send('Delete not done: ' + error.message);
+    }
+});
 
-connectDB().then(()=>{
-    console.log('database connection successfullyyyyyyyyyy');
+app.patch('/user/:userId', async (req, res) => {
+    const userId = req.params?.userId;
+    const data = req.body;
+
+    try {
+        const ALLOWED_UPDATES = ['userId', 'age', 'gender', 'bio', 'photoUrl', 'hobby','skills'];
+        const isUpdateAllowed = Object.keys(data).every((key) => ALLOWED_UPDATES.includes(key));
+
+        if (!isUpdateAllowed) {
+            throw new Error("Update not allowed");
+        }
+
+        const user = await User.findByIdAndUpdate(userId, data, { new: true, runValidators: true });
+
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        if(data?.skills.length>3){
+           
+            throw new Error("more than 3 skills is not allowed")
+        }
+
+        res.send('User updated successfully');
+    } catch (err) {
+        res.status(400).send('Error updating user: ' + err.message);
+    }
+});
+
+connectDB().then(() => {
+    console.log('Database connection successful');
 
     app.listen(7777, () => {
-        console.log('Server is running ');
-      });
-    
-})
-.catch((err)=>{
-    console.log('db cannot connect');
-    
-})
-
-
-
-
-
+        console.log('Server is running');
+    });
+}).catch((err) => {
+    console.log('DB cannot connect: ' + err.message);
+});
--- src/models/user.js
+++ src/models/user.js
@@ -1,30 +1,46 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: "String",
+    required: true,
+    minLength: 3
+    
+  },
+  lastName: {
+    type: "String",
+  },
+  emailId: {
+    type: "String",
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: "String",
+    required: true,
+  },
+  age: {
+    type: "Number",
+  },
+  gender: {
+    type: "String",
+  },
+  photoUrl: {
+    type: "String",
+    default:'https://imgs.search.brave.com/TTaNjijvqIhj4FHkGykQrPUeIro0IKcaaLdROt_g2mM/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hdmF0/YXIuaXJhbi5saWFy/YS5ydW4vcHVibGlj/L2JveQ.jpeg'
+  },
+  bio: {
+    type: "String",
+    default: "spread love!❤️",
+  },
+  hobby: {
+    type: ["String"],
+  },
+  skills:{
+    type: ["String"],
+  }
+},{ timestamps: true });
 
-const userSchema=new mongoose.Schema({
+const User = mongoose.model("User", userSchema);
 
-    firstName:{
-        type:'String'
-    },
-    lastName:{
-        type:'String'
-    },
-    emailId:{
-        type:'String'
-    },
-    password:{
-        type:'String'
-    },
-    age:{
-        type:'Number'
-    },
-    gender:{
-        type:'String'
-    }
-})
-
-
-
-const User=mongoose.model('User',userSchema)
-
-module.exports=User
+module.exports = User;
