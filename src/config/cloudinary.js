8a52de4b2b2c0c258352b953a015cddf0613ecc8
Akshay Jyothi.P <akshayjyothip@gmail.com>
2025-03-27 00:33:16 +0530

    cloudinary secrets

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -3,9 +3,9 @@ const cloudinary = require('cloudinary').v2;
 
  // Configuration
  cloudinary.config({ 
-    cloud_name: 'dorodq6pi', 
-    api_key: '271265886358437', 
-    api_secret: 'tTybTWrzwzxlnJVs8UcS_QrQZIw' // Click 'View API Keys' above to copy your API secret
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, // Click 'View API Keys' above to copy your API key
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
 });
 
 module.exports=cloudinary
