3381a5d129dc2b62f1e38c8200d9edd0217c220c
Akshay Jyothi.P <akshayjyothip@gmail.com>
2025-04-01 20:31:19 +0530

    webhook aws

--- src/utils/razorpay.js
+++ src/utils/razorpay.js
@@ -2,7 +2,9 @@ const Razorpay = require("razorpay");
 
 var instance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
+  
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
+console.log("key_id", process.env.RAZORPAY_KEY_ID),
 
 module.exports = instance;
